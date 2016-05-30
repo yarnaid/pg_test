7dd3d076bc73ea5432517896fab62574ead09f0c
yarnaid <inaiden@dengabank.ru>
2016-05-30 22:39:28 +0300

    can write

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+from random import randint
+from datetime import datetime, timedelta
 from tornado.options import define, options
+from tornado import gen, ioloop
 
 import models
 
@@ -9,8 +12,9 @@ define('db_user', default='postgres', help='User for database')
 define('db_password', default='123456', help='Database password')
 define('test_time', default=60, help='Time of test running in seconds')
 define('clients_number', default=8, help='Number of clients for concurrent testing')
-define('requests_number', default=60, help='Number of request for session for one client')
+define('freq', default=60, help='Frequency of requests generation for each client (items/sec)')
 define('conf_file', default=None, help='Path to configuration file')
+define('rw_ratio', default=0.2, help='Ratio read_req/write_req')
 
 
 options.parse_command_line()
@@ -18,10 +22,52 @@ if options.conf_file is not None and os.path.exists(options.conf_file):
     options.parse_config_file(options.conf_file)
 
 
+@gen.coroutine
+def write_worker():
+    engine = models.get_engine(**options.as_dict())
+    session = models.get_session(engine)
+    models.TestTable.random(session)
+    session.commit()
+    session.close()
+
+
+@gen.coroutine
+def read_worker():
+    engine = models.get_engine(**options.as_dict())
+    session = models.get_session(engine)
+    query = session.query(models.TestTable)\
+        .order_by(models.TestTable.value)\
+        .filter(models.TestTable.value > models.TestTable._rand_lim)
+    for inst in query:
+        inst
+    session.close()
+
+
+@gen.coroutine
+def run():
+    start_time = datetime.now()
+    stop_time = start_time + timedelta(seconds=options.test_time)
+    pause = 60. / options.freq
+    while datetime.now() < stop_time:
+        gen.sleep(pause)
+        p = randint(0, 100)
+        if float(p) / 100. <= options.rw_ratio:
+            yield write_worker()
+        else:
+            yield read_worker()
+
+
 def main():
     engine = models.get_engine(**options.as_dict())
     models.Base.metadata.create_all(engine)
-    raise NotImplementedError
+    session = models.get_session(engine)
+    models.TestTable.random(session)
+    session.commit()
+    session.close()
+    import logging
+    logging.basicConfig()
+    io_loop = ioloop.IOLoop.current()
+    io_loop.run_sync(run)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,11 @@
+from datetime import date, timedelta
+from uuid import uuid4
+from random import randint
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 Base = declarative_base()
 
@@ -9,9 +14,27 @@ def get_engine(**kwargs):
     return create_engine('postgresql://{db_user}:{db_password}@{host}/{db_name}'.format(**kwargs))
 
 
+def get_session(engine):
+    Session = sessionmaker(bind=engine)
+    return scoped_session(Session)()
+
+
 class TestTable(Base):
     __tablename__ = 'test_table'
+
+    _rand_lim = 100000
+    _day_shift = 365
 
     id = Column(Integer, primary_key=True)
     str_field = Column(String)
     dt_field = Column(Date)
+    value = Column(Integer)
+
+    @classmethod
+    def random(cls, session):
+        day = date.today() - timedelta(randint(-cls._day_shift, cls._day_shift))
+        str_ = uuid4()
+        val = randint(-cls._rand_lim, cls._rand_lim)
+        res = cls(str_field=str_, dt_field=day, value=val)
+        session.add(res)
+        return res
